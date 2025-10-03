ff23e5602fef3b8430bb33a765f9e12d32074b8e
Beatriz-Barbosa287 <bgb.beatrizgabrielabarbosa@gmail.com>
2025-10-03 08:38:08 -0300

    03/10/25-> Início de exemplo de GPS

--- Hardware/exemplo_gps/lib/main.dart
+++ Hardware/exemplo_gps/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-
-void main(){
-  runApp(MaterialApp(
-    home: LocationScreen(),
-  ));
+void main() {
+  runApp(MaterialApp(home: LocationScreen()));
 }
 
 class LocationScreen extends StatefulWidget {
@@ -20,17 +17,55 @@ class _LocationScreenState extends State<LocationScreen> {
   String mensagem = "";
 
   //método para Pegar a Localização
-  Future<String> _getLocation() async{
+  Future<String> _getLocation() async {
     bool serviceEnable;
     LocationPermission permission;
 
- //Teste se o Serviço está ativo
+    //Teste se o Serviço está ativo
     serviceEnable = await Geolocator.isLocationServiceEnabled();
-  }
+    if (!serviceEnable) {
+      return Future.error("Serviço de Localização desativado");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Permissão negada");
+      }
+    }
 
-  
+    //se localização liberada 
+    Position position = await Geolocator.getCurrentPosition();
+    return "Lat: ${position.latitude} - Long: ${position.longitude}";
+  }
+  @override 
+  void initState() {
+    //todo: implement initState
+    super.initState();
+    //chama o metodo ates de "buildar" a tela 
+    setState(() {
+      mensagem = _getLocation().toString();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GPS -> LOCALIZAÇÃO"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(mensagem),
+            ElevatedButton(onPressed: ()async{
+              setState(() {
+                mensagem = _getLocation().toString();
+              });
+            }, child: Text("Pegar a Localização"))          
+          ],
+        ),
+      ),
+    );
   }
 }
